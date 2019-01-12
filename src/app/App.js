b3b9916debf78103e27160d6e0638c31084a1e5c
Andrey Leonardo <aleonard@thoughtworks.com>
2019-01-12 12:10:08 -0200

    Updates application style

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './search-bar/SearchBar';
+import SearchResult from './search-result/SearchResult';
+import stubData from '../data.json';
 import './App.scss';
 
-const App = () => (
-  <div className="app">
-    <header className="app__header row">
-      <h1>Experience Finder</h1>
-    </header>
-    <main className="row center-xs">
-      <div className="col-xs-6">
-        <input placeholder="batata" />
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: []
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange({ target: { value } }) {
+    this.setState((state) => {
+      const results = this.findTours(value);
+      return {
+        ...state,
+        results
+      };
+    });
+  }
+
+  findTours(query) {
+    const { data } = this.props;
+    return data.tours.filter(tour => tour.title.toLowerCase().match(query.toLowerCase()));
+  }
+
+  render() {
+    const { results } = this.state;
+    return (
+      <div className="app">
+        <header className="app__header">
+          <h1>Experience Finder</h1>
+        </header>
+        <main className="app__main row center-xs">
+          <SearchBar onChange={this.onChange} />
+          <div className="col-xs-12">
+            { results.map(result => (
+              <SearchResult key={result.title} result={result} />
+            )) }
+          </div>
+        </main>
       </div>
-    </main>
-  </div>
-);
+    );
+  }
+}
+
+App.propTypes = {
+  data: PropTypes.shape({
+    tours: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string
+      })
+    )
+  })
+};
+
+App.defaultProps = {
+  data: stubData
+};
 
 export default App;
--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const app = shallow(<App />);
-  expect(app).toMatchSnapshot();
+describe('App', () => {
+  it('renders without crashing', () => {
+    const app = shallow(<App />);
+    expect(app).toMatchSnapshot();
+  });
 });
